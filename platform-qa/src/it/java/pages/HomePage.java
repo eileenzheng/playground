b443fa8b58de3d6dd480d0a3c0a23f7504e2c7b7
Scott Argenziano <scott.argenziano@vitals.com>
2013-09-16 11:17:16 -0400

    Added link to constants

--- platform-qa/src/it/java/pages/HomePage.java
+++ platform-qa/src/it/java/pages/HomePage.java
@@ -1,8 +1,10 @@
 package pages;
 
+import helpers.Constants;
 import org.openqa.selenium.WebDriver;
 import org.seleniumhq.selenium.fluent.FluentWebElement;
 
+import static org.openqa.selenium.By.cssSelector;
 import static org.openqa.selenium.By.id;
 
 public class HomePage extends BasePage {
@@ -12,7 +14,16 @@ public class HomePage extends BasePage {
     }
 
     public void go() {
+        get(Constants.TEST_SITE);
 
+    }
+
+    public void clickDemoFacilitySearch() {
+        link(cssSelector(".container>div:nth-child(2)>a")).click();
+    }
+
+    public void clickDemoFacilityProfile() {
+        link(cssSelector(".container>div:nth-child(3)>a")).click();
     }
 
     public FluentWebElement imLookingForADoctor() {
