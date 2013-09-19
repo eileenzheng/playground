8917b3f78184b053c05f2226dd2ee0a664850dd1
Scott Argenziano <scott.argenziano@vitals.com>
2013-09-19 10:35:55 -0400

    Initial commit

---
+++ capital-uat/src/main/java/com/capital/WebDriverSingleton.java
@@ -0,0 +1,50 @@
+package com.capital;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.Augmenter;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+public class WebDriverSingleton {
+
+    private static WebDriver driver;
+    private static WebDriver augmentedDriver;
+
+    public static WebDriver getInstance() {
+        if (driver == null) {
+            driver = new FirefoxDriver();
+        }
+        return driver;
+    }
+
+    public static WebDriver getAugmentedDriver() {
+        return augmentedDriver;
+    }
+
+    public static WebDriver getRemoteInstance() {
+        if (driver == null) {
+            URL server = null;
+            try {
+                server = new URL("http://thvitdatadev01.mdx.med:4444/wd/hub");
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            DesiredCapabilities caps = DesiredCapabilities.firefox();
+            driver = new RemoteWebDriver(server, caps);
+            driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+            driver.manage().window().maximize();
+
+            augmentedDriver = new Augmenter().augment(driver);
+        }
+        return driver;
+    }
+
+    public static WebDriver getDriver() {
+        return driver;
+    }
+}
