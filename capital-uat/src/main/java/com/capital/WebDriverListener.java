a52180053908d79c15d33fc4728d993ded053ec3
Scott Argenziano <scott.argenziano@vitals.com>
2013-09-25 11:20:39 -0400

    Added screenshot on failure

--- capital-uat/src/main/java/com/capital/WebDriverListener.java
+++ capital-uat/src/main/java/com/capital/WebDriverListener.java
@@ -1,9 +1,15 @@
 package com.capital;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.testng.IInvokedMethod;
-import org.testng.IInvokedMethodListener;
-import org.testng.ITestResult;
+import org.testng.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.GregorianCalendar;
 
 public class WebDriverListener implements IInvokedMethodListener {
 
@@ -26,11 +32,45 @@ public class WebDriverListener implements IInvokedMethodListener {
 
     @Override
     public void afterInvocation(IInvokedMethod method, ITestResult testResult) {
+        if (testResult.getStatus() == 3) {
+            throw new SkipException("!!! Test method was skipped");
+        }
+
+        if (!testResult.isSuccess()) {
+
+            // Take screenshot
+            File scrFile = ((TakesScreenshot) DriverManager.getAugmentedDriver())
+                    .getScreenshotAs(OutputType.FILE);
+
+            // Make the file name
+            String date = new SimpleDateFormat("MM-dd-yyyy_HHssSSS").format(new GregorianCalendar().getTime());
+            String failureImageFileName = testResult.getMethod().getMethodName() + "_on_" + date + ".png";
+
+
+            File failureImageFile = new File(failureImageFileName);
+
+            try {
+                FileUtils.moveFile(scrFile, failureImageFile);
+                FileUtils.moveFileToDirectory(failureImageFile, new File("target/surefire-reports/html/testfailureimages/"),true);
+            } catch (IOException e) {
+                // Nothing to catch
+            }
+
+            reportLogScreenshot(failureImageFile, date, testResult.getMethod().getMethodName());
+        }
+
         if (method.isTestMethod()) {
             WebDriver driver = DriverManager.getDriver();
             if (driver != null) {
                 driver.quit();
             }
         }
+    }
+
+    protected void reportLogScreenshot(File file, String date, String methodName) {
+        System.setProperty("org.uncommons.reportng.escape-output", "false");
+
+        Reporter.log("<a href=\"testfailureimages/" + file + "\"><p align=\"left\">Error for " + methodName + " screenshot at " + date + "</p>");
+        Reporter.log("<p><img width=\"1024\" src=\"testfailureimages/" + file  + "\" alt=\"screenshot at " + date + "\"/></p></a><br />");
     }
 }
