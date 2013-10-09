3efe7d7fade103e27ea46689a6d20126d3f67142
Scott Argenziano <scott.argenziano@vitals.com>
2013-10-09 11:45:20 -0400

    Time out faster

--- capital-uat/src/main/java/com/capital/DriverFactory.java
+++ capital-uat/src/main/java/com/capital/DriverFactory.java
@@ -45,7 +45,7 @@ public class DriverFactory {
         caps.setBrowserName(System.getenv("SELENIUM_BROWSER"));
         caps.setVersion(System.getenv("SELENIUM_VERSION"));
         caps.setCapability(CapabilityType.PLATFORM, System.getenv("SELENIUM_PLATFORM"));
-
+        caps.setCapability("idle-timeout",10);
         String user = System.getenv("SAUCE_USER_NAME");
         String key = System.getenv("SAUCE_API_KEY");
 
