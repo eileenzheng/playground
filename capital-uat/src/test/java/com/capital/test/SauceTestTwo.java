0cecb24f19de64eacd12e59d7920fb016dc4ea54
Scott Argenziano <scott.argenziano@vitals.com>
2013-10-09 09:15:39 -0400

    Sauce debugging.

--- capital-uat/src/test/java/com/capital/test/SauceTestTwo.java
+++ capital-uat/src/test/java/com/capital/test/SauceTestTwo.java
@@ -2,16 +2,26 @@ package com.capital.test;
 
 import com.capital.pages.SpecialtyTypeSearchPage;
 import org.testng.Reporter;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class SauceTestTwo {
+
+    private String url;
+
+    @Parameters({"url"})
+    @BeforeMethod
+    public void setup(String url) {
+        this.url = url;
+    }
 
     @Test
     public void theTest() {
         Reporter.log("THE KEY " + System.getenv("SAUCE_API_KEY"), true);
         SpecialtyTypeSearchPage specialtySearch = new SpecialtyTypeSearchPage();
 
-        specialtySearch.go("https://providerfinder.capbluecross.com");
+        specialtySearch.go(url);
 
         Reporter.log(specialtySearch.getCurrentUrl(), true);
     }
