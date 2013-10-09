50f8d7ee92f7ec6e37636c49f1246c7130938c49
Scott Argenziano <scott.argenziano@vitals.com>
2013-10-09 10:43:11 -0400

    Better sauce handling

--- capital-uat/src/main/java/com/capital/WebDriverListener.java
+++ capital-uat/src/main/java/com/capital/WebDriverListener.java
@@ -16,33 +16,34 @@ public class WebDriverListener implements IInvokedMethodListener {
 
     @Override
     public void beforeInvocation(IInvokedMethod method, ITestResult testResult) {
-        if (method.isTestMethod()) {
-            String driverType = method.getTestMethod().getXmlTest().getAllParameters().get("testLocation") != null
-                    ? method.getTestMethod().getXmlTest().getAllParameters().get("testLocation")
-                    : "";
+        WebDriver driver;
 
-            WebDriver driver = null;
+        if (method.isTestMethod()) {
             // Check if we're using sauce
-//            if (System.getenv("SAUCE_API_KEY") != null) {
-                Reporter.log("I AM MAKING SAUCE",true);
+            if (System.getenv("SAUCE_API_KEY") != null) {
+//                Reporter.log("I AM MAKING SAUCE",true);
                 Reporter.log(System.getenv("SAUCE_API_KEY"),true);
                 Reporter.log(System.getenv("SAUCE_USER_NAME"),true);
                 driver = DriverFactory.createSauceInstance();
                 DriverManager.setAugmentedWebDriver(driver);
 
-//            } else {
+                // Output Session to Stdout for Sauce Plugin
+                if(testResult.getInstance() != null) printSessionId(testResult.getMethod().getMethodName());
+
+            } else {
 //                Reporter.log("NO SAUCE",true);
-//                driver = driverType.equals("remoteWD")
-//                        ? DriverFactory.createRemoteInstance("firefox")
-//                        : DriverFactory.createLocalInstance("firefox");
-//
-//                if (driverType.equals("remoteWD")) DriverManager.setAugmentedWebDriver(driver);
-//
-//            }
+                String driverType = method.getTestMethod().getXmlTest().getAllParameters().get("testLocation") != null
+                        ? method.getTestMethod().getXmlTest().getAllParameters().get("testLocation")
+                        : "";
+
+                driver = driverType.equals("remoteWD")
+                        ? DriverFactory.createRemoteInstance("firefox")
+                        : DriverFactory.createLocalInstance("firefox");
+
+                if (driverType.equals("remoteWD")) DriverManager.setAugmentedWebDriver(driver);
+            }
 
             DriverManager.setWebDriver(driver);
-
-            if(testResult.getInstance() != null) printSessionId(testResult.getMethod().getMethodName());
 
         }
     }
@@ -52,11 +53,6 @@ public class WebDriverListener implements IInvokedMethodListener {
         if (testResult.getStatus() == 3) {
             throw new SkipException("!!! Test method was skipped");
         }
-
-        // If we're a sauce test output the id
-//        if (System.getenv("SAUCE_API_KEY") != null)
-
-
 
         if (!testResult.isSuccess()) {
 
@@ -95,15 +91,15 @@ public class WebDriverListener implements IInvokedMethodListener {
     protected void reportLogScreenshot(File file, String date, String methodName, String FailedURL) {
         System.setProperty("org.uncommons.reportng.escape-output", "false");
 
-        Reporter.log("<a href=\"testfailureimages/" + file + "\"><p align=\"left\">Error for " + methodName + " screenshot at " + date + "</p>");
         Reporter.log("<p align=\"left\">URL At Failure: " + FailedURL + "</p>");
+        Reporter.log("<a href=\"testfailureimages/" + file + "\"><p align=\"left\">Error for " + methodName + " screenshot at " + date + "</p>");
         Reporter.log("<p><img width=\"768\" src=\"testfailureimages/" + file  + "\" alt=\"screenshot at " + date + "\"/></p></a><br />");
     }
 
     private void printSessionId(String methodName) {
         String id = ((RemoteWebDriver) DriverManager.getDriver()).getSessionId().toString();
-        Reporter.log("ID> " + id,true);
-        String message = String.format("SauceOnDemandSessionID=%1$s job-name=%2$s", ((RemoteWebDriver) DriverManager.getDriver()).getSessionId().toString(), methodName);
+        //Reporter.log("ID> " + id,true);
+        String message = String.format("SauceOnDemandSessionID=%1$s job-name=%2$s", id, methodName);
         System.out.println(message);
     }
 }
