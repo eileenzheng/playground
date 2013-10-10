717a22d2048bb5a60c02caa79a093d1a92034781
Scott Argenziano <scott.argenziano@vitals.com>
2013-10-10 16:12:43 -0400

    Final touches

--- capital-uat/src/main/java/com/capital/DriverManager.java
+++ capital-uat/src/main/java/com/capital/DriverManager.java
@@ -2,12 +2,11 @@ package com.capital;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.Augmenter;
-import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class DriverManager {
     private static ThreadLocal<WebDriver> webDriver = new ThreadLocal<WebDriver>();
     private static ThreadLocal<WebDriver> augmentedDriver = new ThreadLocal<WebDriver>();
-    private static ThreadLocal<String> jobId = new ThreadLocal<String>();
+
     public static WebDriver getDriver() {
         return webDriver.get();
     }
