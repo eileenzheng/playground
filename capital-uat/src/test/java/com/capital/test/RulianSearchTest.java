ab362a22b4c053529838041533caf01420726012
Scott Argenziano <scott.argenziano@vitals.com>
2013-10-14 12:05:55 -0400

    Cleanup

--- capital-uat/src/test/java/com/capital/test/RulianSearchTest.java
+++ capital-uat/src/test/java/com/capital/test/RulianSearchTest.java
@@ -1,76 +1,47 @@
 package com.capital.test;
 
-import com.capital.pages.ResultsPage;
-import com.capital.pages.SearchByNameSearchPage;
 import org.testng.Assert;
 import org.testng.annotations.*;
 import org.w3c.dom.Document;
-import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.net.URL;
+import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- * Capital Search Test Suite
+ * Postgres/Solr test
+ * Queries postgresdb for list of provider names that end in y,s,i,e and then tries to find
+ * the provider in Solr. If Solr returns no results the test will fail.
+ *
+ * pass -DqueryLimit={limit} in the mvn command to set the postgres query limit. If no limit set it will default to 1
  */
 public class RulianSearchTest {
 
-    private String url;
-
-    @Parameters({"url"})
-    @BeforeMethod
-    public void setup(String url) throws Exception {
-        this.url = url;
-    }
-
-    @AfterMethod
-    public void shutdown() {
-    }
-
-//    @Test (dataProvider = "providers")
-//    public void userSearchTest(String name, String zip) {
-//        SearchByNameSearchPage searchByNameSearchPage = new SearchByNameSearchPage();
-//
-//        searchByNameSearchPage.get(url);
-//        searchByNameSearchPage.enterProviderName(name);
-//        searchByNameSearchPage.enterProviderLocation(zip);
-//        searchByNameSearchPage.clickSearchButton();
-//
-//        ResultsPage results = new ResultsPage();
-//        Assert.assertNotEquals(results.getTotalResultsText().trim(),"0 profiles found", "Not found: " + name + " " + zip);
-//
-//    }
-
-
-    @DataProvider(name = "providers", parallel = true)
-    public Object[][] generateProviders() {
-        int limit = System.getProperty("queryLimit") != null ? Integer.parseInt(System.getProperty("queryLimit")) : 1;
-        return dbQuery(Integer.toString(limit));
-
-    }
-
     @Test (dataProvider = "providers")
-    public void userSearchTest(String name, String state) throws Exception {
+    public void userSearchTest(String name, String state) {
 
         String base = "(account_store:capital AND provider_type_store:physician AND (last_name_search:\"%s\"^5 full_name_search:\"%s\"~10) AND state_match:\"%s\")";
         String formatted = String.format(base, name, name, state);
-        formatted = URLEncoder.encode(formatted, "UTF-8");
-//        String url = "http://10.0.4.23:8080/solr/contracts/select/?q=" + formatted;
-        String url = "http://10.0.4.23:8080/solr/contractsalt/select/?q=" + formatted;
 
-        //System.out.println(url);
+        try {
+            formatted = URLEncoder.encode(formatted, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        String url = "http://10.0.4.23:8080/solr/contracts/select/?q=" + formatted;
+        // Use below if you want to see currently generated stuff -- Good when solr job is building
+        // String url = "http://10.0.4.23:8080/solr/contractsalt/select/?q=" + formatted;
+
         com.capital.DriverManager.getDriver().get(url);
+
         try {
             String xml = com.capital.DriverManager.getDriver().getPageSource();
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
@@ -80,7 +51,6 @@ public class RulianSearchTest {
             doc.getDocumentElement().normalize();
 
             NodeList nList = doc.getElementsByTagName("result");
-//            System.out.println("Results :" + nList.item(0).getAttributes().getNamedItem("numFound").getTextContent());
             String result = nList.item(0).getAttributes().getNamedItem("numFound").getTextContent();
             Assert.assertNotEquals(result,"0","No results found for: " + name + " in State: " + state);
 
@@ -90,18 +60,20 @@ public class RulianSearchTest {
 
     }
 
-    // name?criteria%5Bprovider-type%5D=physician&criteria%5Bprovider-name%5D=smith&criteria%5Bprovider-location%5D=New+York%2C+NY&criteria%5Bprovider-plan%5D=All
+    @DataProvider(name = "providers", parallel = true)
+    public Object[][] generateProviders() {
+        int limit = System.getProperty("queryLimit") != null ? Integer.parseInt(System.getProperty("queryLimit")) : 1;
+        return dbQuery(Integer.toString(limit));
 
+    }
 
-    //%5B = [  %5D = ]
-    // ?criteria[provider-type]=physician&criteria[provider-name]=smith&criteria[provider-location]=New York, NY&criteria[provider-plan]=All
     public Object[][] dbQuery(String limit) {
         Connection con = null;
         Statement st = null;
         ResultSet rs = null;
 
         Object[][] result = null;
-
+//        Query using zip code
 //        String query = "select distinct(first_name || ' ' || last_name)as name, pp.default_postal_code  from mdx_core.provider p\n" +
 //                "join mdx_core.provider_practice pp using(provider_id)\n" +
 //                "where (p.last_name ~* '[ysie]$' and length(p.last_name) <= 5) OR (p.first_name ~* '[ysie]$' and length(p.first_name) <= 5) \n" +
@@ -168,4 +140,18 @@ public class RulianSearchTest {
 
         return result;
     }
+
+//    @Test (dataProvider = "providers")
+//    public void userSearchTest(String name, String zip) {
+//        SearchByNameSearchPage searchByNameSearchPage = new SearchByNameSearchPage();
+//
+//        searchByNameSearchPage.get(url);
+//        searchByNameSearchPage.enterProviderName(name);
+//        searchByNameSearchPage.enterProviderLocation(zip);
+//        searchByNameSearchPage.clickSearchButton();
+//
+//        ResultsPage results = new ResultsPage();
+//        Assert.assertNotEquals(results.getTotalResultsText().trim(),"0 profiles found", "Not found: " + name + " " + zip);
+//
+//    }
 }
