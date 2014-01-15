237896d040d668d36da5d7f06eff219775080449
Eileen Zheng <Eileen.Zheng@vitals.com>
2014-01-15 10:01:57 -0500

    increase expected result since default is now 15 miles

--- vitals/src/test/java/com/vitals/test/SearchTest.java
+++ vitals/src/test/java/com/vitals/test/SearchTest.java
@@ -165,7 +165,7 @@ public class SearchTest {
 
         SearchResultsPage results = homePage.header.clickFirstSpecialty();
        
-        m_assert.assertTrue((results.getResultsCountNumber()>1500 && results.getResultsCountNumber() <2000), 
+        m_assert.assertTrue((results.getResultsCountNumber()>1500 && results.getResultsCountNumber() <2500), 
         		"# of result for Cardiologists in New York not within expected range! ");     
         int count = results.getResultsCountNumber();
         Reporter.log(count + " results with default filter settings");
