cd160dbf8d2abe58bd2ed003721f5906f86fa687
Eileen Zheng <Eileen.Zheng@vitals.com>
2014-07-02 13:49:14 -0400

    update ucc 0 result url

--- vitals/src/test/java/com/vitals/test/UccTest.java
+++ vitals/src/test/java/com/vitals/test/UccTest.java
@@ -100,12 +100,12 @@ public class UccTest {
         m_assert = new SoftAssert();
 
         UccSearchResultsPage serp = new UccSearchResultsPage();
-        serp.get(url + "/urgent-care/ak/kenai");
+        serp.get(url + "/urgent-care/hi/waimea");
 
         m_assert.assertTrue(serp.searchSentenceNoResult().get(0).getText().toString().equals("We found 0"), "Search sentence is wrong");
         m_assert.assertTrue(serp.searchSentenceNoResult().get(1).getText().toString().equals("Urgent Care Centers"), "Search sentence is wrong");
         m_assert.assertTrue(serp.searchSentenceNoResult().get(2).getText().toString().equals("near"), "Search sentence is wrong");
-        m_assert.assertTrue(serp.searchSentenceNoResult().get(3).getText().toString().equals("Kenai, AK"), "Search sentence is wrong");
+        m_assert.assertTrue(serp.searchSentenceNoResult().get(3).getText().toString().equals("Waimea, HI"), "Search sentence is wrong");
         m_assert.assertTrue(serp.h2().getText().toString().equals("Closest similar results"), "Closest sentence is wrong");
         m_assert.assertTrue(serp.searchResults().size()>0 && serp.searchResults().size()<26, "Not showing 1 - 25 results");
 
