a0393a84ee109ab868e3bb8a94e5c3b4ed1cd347
Eileen Zheng <Eileen.Zheng@vitals.com>
2015-01-23 11:17:46 -0500

    force update wlw case

--- vitals/src/test/java/com/vitals/test/WlwTests.java
+++ vitals/src/test/java/com/vitals/test/WlwTests.java
@@ -10,7 +10,7 @@ import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
-public class WlwTest {
+public class WlwTests {
 
     private String url;
     private SoftAssert m_assert;
