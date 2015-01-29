8f16d4c9d63a26fe7bd1e4a2e2444e2fa03b51a4
Eileen Zheng <Eileen.Zheng@vitals.com>
2015-01-29 14:03:50 -0500

    update sitemap

--- vitals/src/main/java/com/vitals/pages/sitemap/LocationsPage.java
+++ vitals/src/main/java/com/vitals/pages/sitemap/LocationsPage.java
@@ -7,10 +7,10 @@ import static org.openqa.selenium.By.cssSelector;
 public class LocationsPage extends BasePage {
 
     public FluentWebElements specialties() {
-        return links(cssSelector("div.row.location:first-of-type ul>li>a"));
+        return links(cssSelector(".location-row:first-of-type li>a"));
     }
 
     public FluentWebElements groupPractices() {
-        return links(cssSelector("div.row:last-of-type ul>li>a"));
+        return links(cssSelector(".location-row:last-of-type li>a"));
     }
 }
