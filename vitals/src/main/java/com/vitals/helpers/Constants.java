8f354372bbcdb5e9954b9a3bb01953d3272724d1
Eileen Zheng <Eileen.Zheng@vitals.com>
2015-03-17 17:01:34 -0400

    increase implicit time out for IE sauce

--- vitals/src/main/java/com/vitals/helpers/Constants.java
+++ vitals/src/main/java/com/vitals/helpers/Constants.java
@@ -2,7 +2,7 @@ package com.vitals.helpers;
 
 public interface Constants {
 
-    static public final int SELENIUM_IMPLICIT_WAIT = 5;
+    static public final int SELENIUM_IMPLICIT_WAIT = 30;
     static public final int SELENIUM_EXPLICIT_WAIT = 5;
 
 }
